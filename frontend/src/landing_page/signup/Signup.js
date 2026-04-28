c6c876a79ba4c298d16c864896ef37a1bb16572c
Shruti Patnaik <patnaik.shruti123@gmail.com>
2026-04-29 04:49:11 +0530

    Initial commit - Zerodha clone

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Signup() {
-    return ( 
-        <h1>Signup </h1>
-     );
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    localStorage.setItem("token", "dummy123");
+
+    // 🔥 dashboard open (different app)
+    window.location.href = "http://localhost:3000";
+  };
+
+  return ( 
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Signup</h2>
+
+      <input
+        type="text"
+        placeholder="Enter username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Enter password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleSignup}>Create Account</button>
+    </div>
+  );
 }
 
 export default Signup;
