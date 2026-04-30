d2f405002b3724294c908625e66d80e6aea4f533
Shruti Patnaik <patnaik.shruti123@gmail.com>
2026-05-01 05:18:40 +0530

    Fixed chart rename, watchlist update and frontend changes

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -14,7 +14,8 @@ import {
 } from "@mui/icons-material";
 
 import { watchlist } from "../data/data";
-import { DoughnutChart } from "./DoughnoutChart";
+
+import { DoughnutChart } from "./DoughnutChart";
 
 const labels = watchlist.map((subArray) => subArray["name"]);
 
