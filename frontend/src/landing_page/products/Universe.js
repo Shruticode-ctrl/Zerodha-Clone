3e948e26d1ab9f9b2987f5746da4748a8af708d5
Shruti Patnaik <patnaik.shruti123@gmail.com>
2026-05-03 03:37:08 +0530

    Fixed ESLint errors (href, img alt, Navbar/Footer)

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -8,32 +8,32 @@ function Universe() {
       <p className="text-muted">Extend your trading and investment experience even further with our partner platforms</p>
 
         <div className="col-4 p-3 mt-5">
-          <img src="media/smallcaseLogo.png"/>
+          <img src="media/smallcaseLogo.png" alt="small case logo"/>
           <p className="text-small text-muted">Thematic investment platform</p>
         </div>
 
          <div className="col-4 p-3 mt-5">
-          <img src="media/zerodhaFundhouse.png" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
+          <img src="media/zerodhaFundhouse.png" alt="fund house" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
           <p className="text-small text-muted">Asset management</p>
         </div>
 
          <div className="col-4 p-3 mt-5">
-          <img src="media/streakLogo.png" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
+          <img src="media/streakLogo.png" alt="streak logo" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
           <p className="text-small text-muted"> Algo & strategy platform</p>
         </div>
 
             <div className="col-4 p-3 mt-5">
-          <img src="media/sensibullLogo.svg" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
+          <img src="media/sensibullLogo.svg" alt="sensibull logo" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
           <p className="text-small text-muted">Options trading platform</p>
         </div>
           
            <div className="col-4 p-3 mt-5">
-          <img src="media/goldenpiLogo.png"/>
+          <img src="media/goldenpiLogo.png" alt="golden pi"/>
           <p className="text-small text-muted">Bonds trading platform</p>
         </div>
 
            <div className="col-4 p-3 mt-5">
-          <img src="media/dittoLogo.png" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
+          <img src="media/dittoLogo.png" alt="ditto logo" style={{ height: "40px", width: "100%", objectFit: "contain" }}/>
           <p className="text-small text-muted">Insurance</p>
         </div>
         <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%",margin:"0 auto"}}>Sign up for free</button>
